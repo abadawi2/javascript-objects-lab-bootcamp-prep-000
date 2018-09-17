d7f52ebcc7f07458d6ada9e46600d22986e12c8b
abadawi2 <abadawi@terpmail.umd.edu>
2018-09-17 20:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
 var recipes = {key:'value'}
-
-function updateObjectWithKeyAndValue(object,key,value){
-  object.key= 'value';
-  return object;
-}
-
-function updateObjectWithKeyAndValue(object,key,value){
-  object.key= 'value';
-  var newObj = Object.assign({}, recipes,object)
-  return newObj;
-}
-
-function updateObjectWithKeyAndValue(object,key,value){
-  recipes[key]= 'value';
-  return object;
-}
